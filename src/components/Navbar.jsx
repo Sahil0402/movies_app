7e310fb49ef136bf3dbad45e7f3cb796d6612ba2
Sahil Shendurkar <sahilshendurkar130@gmail.com>
2024-02-01 00:30:00 +0530

    Readme updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">TV Shows App</Link>
+        <Link className="navbar-brand" to="/">Movies Mania</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
