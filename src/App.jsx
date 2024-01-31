cf8405c9bf3a3aedad9ec51409bda72d3780792b
Sahil Shendurkar <sahilshendurkar130@gmail.com>
2024-02-01 01:17:00 +0530

    App.jsx error fixed and css changes are made

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShowList from "./components/ShowList";
 import ShowDetail from "./components/ShowDetail";
 import AllBookings from "./components/AllBookings";
-import NavBar from "./components/NavBar"; // Import the NavBar component
+import NavBar from "./components/NavBar"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 function App() {
   return (
     <Router>
-      <NavBar /> {/* Include the NavBar component */}
+      <NavBar /> 
       <Routes>
         <Route path="/" element={<ShowList />} />
         <Route path="/show/:id" element={<ShowDetail />} />
